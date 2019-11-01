8020b20a4ea3052ac48e61a8d43edf35d2da4bc8
Thomas Wolter <itsthomas@outlook.com>
2019-11-01 12:21:54 +0000

    generated-build

---
+++ build/precache-manifest.09c6a188655574eedabc2a73df9cdfa5.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "127042f9c08f57d3cbb6a1d4819e5034",
+    "url": "/index.html"
+  },
+  {
+    "revision": "732a1f3f2aaefd78caf3",
+    "url": "/static/js/bundle.js"
+  }
+]);
